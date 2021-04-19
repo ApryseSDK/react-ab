1200e4c7cba205666b69922c29dbca193c92275d
Liam Ross <liamross.code@gmail.com>
2021-04-19 15:23:45 -0700

    feat: implement hook in ABTest

--- src/hooks/useExperiment.ts
+++ src/hooks/useExperiment.ts
@@ -1,29 +1,42 @@
-import { useCallback, useContext, useEffect, useRef, useState } from "react";
-import { BackendContext, SSRContext } from "../context";
-import { ABService } from "../service";
+import { useCallback, useContext, useEffect, useMemo, useRef, useState } from 'react';
+import { BackendContext, SSRContext } from '../context';
+import { ABService } from '../service';
+
+export interface UseExperimentValue {
+  /** The variant index to use for the experiment. */
+  variant: number;
+  /** Is loading async client-side variant. */
+  loading: boolean;
+  /** Errors with async client-side variant. */
+  error: string;
+  /** Call to retry a failed client-side variant. */
+  getClientSide: () => Promise<void>;
+  /** Is using server-side values for variant. */
+  isSSREnabled: boolean;
+}
 
 /**
- *
+ * Returns variant number, as well as status if async client-side fetch is used.
  * @param name Name of the experiment. This will be cached, so changing it will
  * have no effect.
  */
-export const useExperiment = (name: string) => {
+export const useExperiment = (name: string): UseExperimentValue => {
   const nameRef = useRef(name);
   const experiment = useRef(ABService.getExperiment(name));
 
   const ssr = useContext(SSRContext);
   const backend = useContext(BackendContext);
 
-  const [variant, setVariant] = useState<number | undefined>(() => {
-    const selectedSSRVariant = ssr?.[nameRef.current];
-    const isSSREnabled = typeof selectedSSRVariant === "number";
-    return isSSREnabled ? selectedSSRVariant : experiment.current.selectedVariant;
-  });
-  const noVariant = typeof variant !== "number";
+  const selectedSSRVariant = ssr?.[nameRef.current];
+  const isSSREnabled = typeof selectedSSRVariant === 'number';
+
+  const [variant, setVariant] = useState<number | undefined>(() =>
+    isSSREnabled ? selectedSSRVariant : experiment.current.selectedVariant
+  );
+  const noVariant = typeof variant !== 'number';
 
   const [loading, setLoading] = useState(noVariant);
   const [error, setError] = useState<string | null>(null);
-
 
   const getClientSide = useCallback(async () => {
     if (!noVariant) return;
@@ -36,28 +49,31 @@ export const useExperiment = (name: string) => {
       ABService.setSelectedIndex(nameRef.current, variantIndex);
       setError(null);
     } catch (error) {
-      setError(String(error) || "unknown error loading variant");
+      setError(String(error) || 'unknown error loading variant');
     }
 
     setLoading(false);
   }, [backend, noVariant]);
 
-  
   useEffect(() => {
     if (noVariant) getClientSide();
   }, [getClientSide, noVariant]);
 
   useEffect(() => {
-    if (ABService.ssrEnabled && typeof variant === "number") {
+    if (ABService.ssrEnabled && typeof variant === 'number') {
       ABService.log(`(SSR) Setting variant for ${nameRef.current} (${experiment.current.id}) to ${variant}`);
       backend?.setVariant(experiment.current.id, variant);
     }
   }, [backend, variant]);
 
-  return {
-    variant,
-    loading,
-    error,
-    getClientSide,
-  };
+  return useMemo<UseExperimentValue>(
+    () => ({
+      variant,
+      loading,
+      error,
+      getClientSide,
+      isSSREnabled,
+    }),
+    [variant, loading, error, getClientSide, isSSREnabled]
+  );
 };
